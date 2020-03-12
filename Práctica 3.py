c1379d08605e77940dee0f07fe08bc67e58a34c0
Irvin Daniel Escutia Arreola <dany_55@hotmail.es>
2020-03-12 13:49:06 -0600

    Update Práctica 3.py

--- Práctica 3.py
+++ Práctica 3.py
@@ -4,29 +4,41 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import mysql.connector as mysql
 
+conex= mysql.connect(host='localhost',user='root',passwd='',db='Webs')
+op= conex.cursor(buffered=True)
+
 def busca(pag):
     html= urlopen(pag)
     res= tk.Tk()
     res.title("Enlaces")
     res.tk_setPalette('light gray')
     print("\nExtraer los enlaces de la página web: "+pag+"\n")
-    ttk.Label(res,text="\nEnlaces extraidos: "+pag+"\n").pack() 
-    bs= BeautifulSoup(html.read(),'html.parser')
-    cad=""
-    enl= 0
-    conex= mysql.connect(host='localhost',user='root',passwd='',db='Webs')
-    op= conex.cursor()
-    for enlaces in bs.find_all("a"):
-        print("href: {}".format(enlaces.get("href")))
-        cad+="href: {}".format(enlaces.get("href")+"\n")
-        enl=+1
-        op.execute('INSERT INTO Enlaces values("'+pag+'",False)')
+    try:
+        op.execute('INSERT INTO Enlaces values("'+pag+'", False)')
+    except:
+        pass
+    op.execute('SELECT * from Enlaces where status=0')
+    conex.commit()
+    for pag, trsh in op:
+        print("\nRevisando "+pag+"\n")
+        try:
+            html= urlopen(pag)
+            bs= BeautifulSoup(html,'html.parser')
+            for link in bs.findAll('a'):
+                print('href: {}'.format(link.get('href')))
+                try:
+                    op.execute('INSERT INTO Enlaces values("'+link.get('href')+'", False)')
+                except:
+                    print("Valor Duplicado")
+        except:
+            print("No se pudo acceder a "+pag+"\n")
+        op.execute("UPDATE Enlaces SET status=1 WHERE pagina='"+pag+"'")
+        conex.commit()
+        print("\nTerminó de Revisar Enlaces de "+pag)
+        op.execute("SELECT * from Enlaces where status=0")
+    print("Revisión de enlaces Finalizada")
+    conex.commit()
     conex.close()
-    print(str(enl)+" enlaces encontrados\n")
-    obj= scrolledtext.ScrolledText(res,width=120,height=20,wrap=tk.WORD)
-    obj.insert(1.0,cad)
-    obj.pack()
-    res.mainloop()
 
 ventana= tk.Tk()
 ventana.title("Web Scraping")
